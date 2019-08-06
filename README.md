7c76b01231c73f8515a4a37fd365888d34f040b6
José Luiz Junior <juninhopc@icloud.com>
2019-08-05 23:48:32 -0300

    Final

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SrtSync
- 
+![SRTSYNC_LOGO](Logo.png)
+
+**SrtSync** is a simple delay editor for .srt files to produce permanent synchronization with movies.
+
+## Example of Usage
+
+* Papillon.srt => Input File
+* -10000 => Delay (microseconds)
+* Papillon.srt => Input File
+
+`ruby srtsync.rb Papillon.srt -10000 PapillonNew.srt`
+
+### Result
+
+![SRTSYNC_USO](Uso.png)
+
+by: [juninhoojl](http://github.com/juninhoojl)
---
+++ srtsync.rb
@@ -0,0 +1,28 @@
+#  !/usr/bin/ruby
+#  Created by:
+#  José Luiz Junior - @juninhoojl
+#  SrtSync
+
+require 'time'
+
+if ARGV.length != 3
+  puts "Not enough arguments to SrtSync\n\t<SubIn.srt> <±Milliseconds> <SubOut.srt>"
+  exit(0)
+end
+
+arquivo = ARGV[0].to_s
+ajuste = ARGV[1].to_f/1000
+saida = ARGV[2].to_s
+linhas = Array.new
+
+File.foreach(arquivo) do |linha|
+	if linha[8] == "," and linha[13] == "-" and linha[14] == "-" and linha[15] == ">"
+		tempa = Time.parse(linha[0..11]) + ajuste
+		tempb = Time.parse(linha[17..27]) + ajuste
+		linha[0..11] = tempa.strftime("%H:%M:%S,%L")
+		linha[17..28] = tempa.strftime("%H:%M:%S,%L")
+	end
+	linhas << linha.chomp
+end
+
+IO.write(saida, linhas.join("\n"))
